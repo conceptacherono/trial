915aca18405d8e0fcd749d69b61a2447545ffe14
conceptacherono <91899034+conceptacherono@users.noreply.github.com>
2022-02-24 01:16:10 +0300

    'navbar'

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 #where we will create all our view functions
 from flask import render_template
 from app import app
+from .request import get_movies
 
 # Views
 @app.route('/')
@@ -9,4 +10,14 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    
+    title = 'Home - Welcome to The best Movie Review Website Online'
+    return render_template('index.html', title = title)
+
+@app.route('/movie/<int:movie_id>')
+def movie(movie_id):
+
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    return render_template('movie.html',id = movie_id)    
